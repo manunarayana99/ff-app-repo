ec6e749ac7302655872560b674294b66144a5892
Maanasa <manunarayana999@gmail.com>
2025-03-08 16:32:18 +0530

    my secondapp first commit

---
+++ secondapp.py
@@ -0,0 +1,18 @@
+str="hello world"
+str2="how are you"
+print(len(str))
+print(str.upper())
+print(str.lower())
+print(str.capitalize())
+print(str.count('l'))
+print(str.title())
+print(str.find('o'))
+print(str.replace('o','a'))
+print(str.isdigit())
+print(str.isalpha())
+print(str.isalnum())
+print(str[1:5])    #slice operator (:)
+print(str[0:8:2])   # slice operation with skip step ----> [start : end : skip]
+print(str[-2:-8:-2])
+print(str[-8:-2:-2])  #
+print(str + str2)  # string concatenation
