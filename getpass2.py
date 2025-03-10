2adf26cdd10d5eb500a195c2359bb89fd02be1d4
Maanasa <manunarayana999@gmail.com>
2025-03-10 19:32:19 +0530

    getpass2 commit1

---
+++ getpass2.py
@@ -0,0 +1,10 @@
+import getpass
+actual_word=getpass.getpass()
+guess_word=input("enter your word")
+if actual_word==guess_word:
+    print("mathched")
+else:
+    print("unmatched")
+    print("unmatched")
+    print("unmatched")
+    print("unmatched")
